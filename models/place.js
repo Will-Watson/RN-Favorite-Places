0a4a16e98111e0603c4b38c2e0fad8d40ffd184e
William Watson <williampwatson1991@gmail.com>
2024-02-22 22:39:48 -0500

    feat: Place class model added to models folder

---
+++ models/place.js
@@ -0,0 +1,10 @@
+class Place {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location; // { lat: ..., lng: ... }
+  }
+}
+
+module.exports = Place;
