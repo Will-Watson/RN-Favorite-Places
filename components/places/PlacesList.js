946b07c7634e9d9589a881f820173e4c8af56d0d
William Watson <williampwatson1991@gmail.com>
2024-02-22 22:14:12 -0500

    feat: places folder added to components folder. PlacesList component created with dummy data

---
+++ components/places/PlacesList.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const PlacesList = () => {
+  return (
+    <View>
+      <Text>All Places</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default PlacesList;
