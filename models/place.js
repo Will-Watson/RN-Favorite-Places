d286b3a9812a4ff50169c084a538d4fe9d9714b6
William Watson <williampwatson1991@gmail.com>
2024-02-29 19:32:29 -0500

    feat: Place model updated to take in an ID field that is just a random string for the time being

--- models/place.js
+++ models/place.js
@@ -4,6 +4,7 @@ class Place {
     this.imageUri = imageUri;
     this.address = address;
     this.location = location; // { lat: ..., lng: ... }
+    this.id = new Date().toString() + Math.random().toString();
   }
 }
 
