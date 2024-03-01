049734e755964f00f1b77335f71da2bdc8b09b41
William Watson <williampwatson1991@gmail.com>
2024-02-29 21:07:07 -0500

    feat: requesting permissions now working with ios simulator even though i have to use my phone....

--- components/places/ImagePicker.js
+++ components/places/ImagePicker.js
@@ -1,8 +1,39 @@
-import { View, Button, StyleSheet } from 'react-native';
-import { launchCameraAsync } from 'expo-image-picker';
+import { View, Button, StyleSheet, Alert } from 'react-native';
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from 'expo-image-picker';
 
 const ImagePicker = () => {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  const verifyPermissions = async () => {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient permissions!',
+        'You need to grant camera permissions to use this app.',
+        [{ text: 'Okay' }]
+      );
+      return false;
+    }
+
+    return true;
+  };
+
   const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
