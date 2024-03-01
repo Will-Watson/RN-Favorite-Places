bea936031ed02e84a1b83d27bc01d7df7e8229df
William Watson <williampwatson1991@gmail.com>
2024-02-29 19:44:10 -0500

    feat: added some more content and styling to placeslist

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -2,6 +2,16 @@ import { FlatList, StyleSheet } from 'react-native';
 import PlaceItem from './PlaceItem';
 
 const PlacesList = ({ places }) => {
+  if (places.length === 0 || !places) {
+    return (
+      <View style={styles.fallBackContainer}>
+        <Text style={styles.fallBackText}>
+          No places found. Maybe start adding some!
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={places}
@@ -11,6 +21,15 @@ const PlacesList = ({ places }) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  fallBackContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fallBackText: {
+    fontSize: 16,
+  },
+});
 
 export default PlacesList;
