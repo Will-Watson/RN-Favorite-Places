79011f14852fb009c5c4e2c7a050c2b5113370a7
William Watson <williampwatson1991@gmail.com>
2024-02-29 19:38:40 -0500

    feat: PlaceItem added to FlatList

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,10 +1,15 @@
-import {FlatList, StyleSheet} from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
+import PlaceItem from './PlaceItem';
 
-const PlacesList = ({places}) => {
+const PlacesList = ({ places }) => {
   return (
-    <FlatList data={places} keyExtractor={(item) => item.id} renderItem={({item}) => {}} />
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem />}
+    />
   );
-}
+};
 
 const styles = StyleSheet.create({});
 
