7a6b64d7bbd00ecdbc93a5c8b5cd5501005710db
William Watson <williampwatson1991@gmail.com>
2024-02-29 20:06:17 -0500

    feat: more structure aded to dummy icon button component

--- components/UI/IconButton.js
+++ components/UI/IconButton.js
@@ -1,8 +1,17 @@
-import { Pressable } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-const IconButton = () => {
-  return <Pressable></Pressable>;
+const IconButton = ({ icon, size, color, onPress }) => {
+  return (
+    <Pressable onPress={onPress}>
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
 };
+
+const styles = StyleSheet.create({
+  button: {},
+  pressed: {},
+});
 
 export default IconButton;
