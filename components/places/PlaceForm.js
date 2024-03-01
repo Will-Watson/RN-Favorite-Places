8c5f00549c466c770f0d379933ed057e8c28d1ca
William Watson <williampwatson1991@gmail.com>
2024-02-29 19:47:07 -0500

    feat: PlaceForm dummy component created and added to components places folder

---
+++ components/places/PlaceForm.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const PlaceForm = () => {
+  return (
+    <View>
+      <Text>Place Form</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default PlaceForm;
