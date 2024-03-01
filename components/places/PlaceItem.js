8b17d72d2f7f76bfb8c8067d54de498368956eda
William Watson <williampwatson1991@gmail.com>
2024-02-29 19:37:27 -0500

    feat: PlaceItem dummy component created for rendering with FlatList. Added to components places folder

---
+++ components/places/PlaceItem.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const PlaceItem = () => {
+  return (
+    <View>
+      <Text>PlaceItem</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default PlaceItem
--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -2,7 +2,7 @@ import {FlatList, StyleSheet} from 'react-native';
 
 const PlacesList = ({places}) => {
   return (
-    <FlatList data={places} />
+    <FlatList data={places} keyExtractor={(item) => item.id} renderItem={({item}) => {}} />
   );
 }
 
