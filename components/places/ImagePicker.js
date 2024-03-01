b7181d9ed2d260f945cafbeb560808ca1aea28b3
William Watson <williampwatson1991@gmail.com>
2024-02-29 20:35:00 -0500

    feat: dummy image picker component added to components places

---
+++ components/places/ImagePicker.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImagePicker = () => {
+  return (
+    <View>
+      <Text>Image Picker</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ImagePicker;
