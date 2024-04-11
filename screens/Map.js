62ba194bb20c3ae626712f024fc7d700e50cac7f
William Watson <williampwatson1991@gmail.com>
2024-04-11 19:07:33 -0400

    feat: MapView component added to map screen. dummy data input for now

--- screens/Map.js
+++ screens/Map.js
@@ -1,10 +1,15 @@
 import { View, Text, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
+  const region = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
   return (
-    <View>
-      <Text>All Places</Text>
-    </View>
+    <MapView initialRegion={region}></MapView>
   );
 };
 
