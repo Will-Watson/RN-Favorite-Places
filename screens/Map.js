6ec82f30b5a694cd405d8dbd01adf3fce17c3393
William Watson <williampwatson1991@gmail.com>
2024-04-11 19:20:18 -0400

    feat: map opening well. Marker tap added

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
+  const [selectedLocation, setSelectedLocation] = useState({
+    lat: null,
+    lng: null,
+  });
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat, lng });
+  };
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation.lat && selectedLocation.lng && (
+        <Marker
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
