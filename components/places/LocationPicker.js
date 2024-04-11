16a4451ece613f02ca79bc26ba1e9e8d203b0125
William Watson <williampwatson1991@gmail.com>
2024-04-11 17:31:00 -0400

    feat: psuedo styling added similar to image picker preview. Colors imported from constants

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -1,21 +1,40 @@
 import { View, Text, StyleSheet } from 'react-native';
 
 import OutlinedButton from '../UI/OutlinedButton';
+import { Colors } from '../../constants/colors';
 
 const LocationPicker = () => {
+  const getLocationHandler = () => {};
+  const pickOnMapHandler = () => {};
+
   return (
     <View>
-      <View >
+      <View>
         <Text>No location chosen yet!</Text>
       </View>
       <View>
-        <OutlinedButton icon='location'>Locate User</OutlinedButton>
-        <OutlinedButton icon='map'>Pick on Map</OutlinedButton>
+        <OutlinedButton icon='location' onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon='map' onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
       </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 8,
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  action: {},
+});
 
 export default LocationPicker;
