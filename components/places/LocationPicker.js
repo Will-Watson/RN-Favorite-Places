221fc9f1af14aa8e4b4947a5af049d6c301aa82f
William Watson <williampwatson1991@gmail.com>
2024-04-11 17:15:59 -0400

    feat: LocationPicker dummy component created

---
+++ components/places/LocationPicker.js
@@ -0,0 +1,15 @@
+import { View, Button, Text, StyleSheet } from 'react-native';
+
+const LocationPicker = () => {
+  return (
+    <View>
+      <View >
+        <Text>No location chosen yet!</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default LocationPicker;
