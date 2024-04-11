9600a07d3a55b31a477ef4591c5ea1ee9f696f5c
William Watson <williampwatson1991@gmail.com>
2024-04-11 17:27:37 -0400

    feat: added outlinedbuttons to dummy component. removed standard button import from React-Native

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -1,10 +1,16 @@
-import { View, Button, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import OutlinedButton from '../UI/OutlinedButton';
 
 const LocationPicker = () => {
   return (
     <View>
       <View >
         <Text>No location chosen yet!</Text>
+      </View>
+      <View>
+        <OutlinedButton icon='location'>Locate User</OutlinedButton>
+        <OutlinedButton icon='map'>Pick on Map</OutlinedButton>
       </View>
     </View>
   );
